22b74259c134f932ce9f541394159b4d2edc2e10
Vladimir Moravec <vmoravec@suse.com>
2013-08-01 17:41:38 +0200

    Ycp should be yast

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,6 @@
 require "fileutils"
 require "test/unit"
 require "mocha/setup"
-require "ycp"
+require "yast"
 
 $LOAD_PATH.unshift File.expand_path(File.join(File.dirname(__FILE__), '../src/modules'))
