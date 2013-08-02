1741c2bec1ac3c59cdfcb71122bfe84f214884dc
Vladimir Moravec <vmoravec@suse.com>
2013-08-02 17:02:07 +0200

    Add rcov formatter to simplecov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,5 @@
 require "simplecov"
+require "simplecov-rcov"
 require "fileutils"
 require "test/unit"
 require "mocha/setup"
@@ -6,5 +7,6 @@ require "yast"
 
 $LOAD_PATH.unshift File.expand_path(File.join(File.dirname(__FILE__), '../src/modules'))
 
+SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
 SimpleCov.start
 
