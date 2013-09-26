37fa5e65773a5bfae6fc8251074e9cf4a6e9e3e4
Vladimir Moravec <vmoravec@suse.com>
2013-09-26 18:08:27 +0200

    Add proposal class which is responsible for making the proposal

--- src/clients/services-manager_proposal.rb
+++ src/clients/services-manager_proposal.rb
@@ -1,14 +1,25 @@
 class ServicesManagerProposal < Yast::Client
   Yast.import 'ServicesManager'
+  Yast.import 'Arch'
+  Yast.import 'Linuxrc'
+  Yast.import 'Mode'
+  Yast.import 'Pkg'
+
+  extend FastGettext::Translation
+
+  DESCRIPTION = {
+    'id'              => 'services-manager',
+    'menu_title'      => N_("&Default systemd target and services"),
+    'rich_text_title' => N_("Default systemd target and services")
+  }
 
   def initialize
-    textdomain 'services-manager'
-    function = WFM.Args.first.to_s
-
+    args = WFM.Args
+    function = args.shift.to_s
     case function
-      when 'MakeProposal' then make_proposal
+      when 'MakeProposal' then Proposal.new(args)
       when 'AskUser'      then ask_user
-      when 'Description'  then description
+      when 'Description'  then DESCRIPTION
       when 'Write'        then write
     end
   end
@@ -16,18 +27,58 @@ class ServicesManagerProposal < Yast::Client
   def ask_user
   end
 
-  # Return a map with 3 items:
-  # id => unique widget id as string
-  # menu_title => translated menu with keyboard shortcut for button widget
-  # rich_text_tile => translated title as plain string
-  def description
-  end
-
-  def make_proposal
-  end
-
   def write
+  end
+
+  class Proposal
+    module Type
+      GRAPHICAL = 'graphical'
+      MULTIUSER = 'multi-user'
+    end
+
+    def initialize args
+      set_default_target
+      make_proposal
+    end
+
+    private
+
+    #TODO
+    # Add warnings to the last 2 setups, add logging
+    # Create the proposal widget as in RunlevelEd
+    def set_default_target
+      if Mode.autoinst
+        # Look at the RunlevelProposal, around line 118 where this stops
+        # check if vnc is running
+        # check if ssh is running
+      else
+        if Arch.x11_setup_needed && Pkg.IsSelected("xorg-x11") || Mode.live_installation
+          default_target = Type::GRAPHICAL
+        end
+        if Linuxrc.serial_console
+          default_target = Type::MULTIUSER
+        elsif Linuxrc.vnc && Linuxrc.usessh
+          if UI.GetDisplayInfo == 'TextMode'
+            default_target = Type::MULTIUSER
+          else
+            default_target = Type::GRAPHICAL
+          end
+        elsif Linuxrc.vnc
+          default_target = Type::GRAPHICAL
+        elsif Linuxrc.usessh
+          default_target = Type::MULTIUSER
+        end
+      end
+      Yast::SystemdTarget.default_target = default_target
+    end
+
+    def
+
+    def make_proposal
+      installation_type =
+    end
   end
 end
 
 ServicesManagerProposal.new
+42
