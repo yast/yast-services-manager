99daaec83f4f18278e0b1a9d4ff9604bb8528974
Vladimir Moravec <vmoravec@suse.com>
2013-10-01 17:16:48 +0200

    Another parts of the proposal

--- src/clients/services-manager_proposal.rb
+++ src/clients/services-manager_proposal.rb
@@ -1,84 +1,97 @@
-class ServicesManagerProposal < Yast::Client
-  Yast.import 'ServicesManager'
-  Yast.import 'Arch'
-  Yast.import 'Linuxrc'
-  Yast.import 'Mode'
-  Yast.import 'Pkg'
+module Yast
+  class ServicesManagerProposal < Client
+    Yast.import 'ServicesManager'
+    Yast.import 'Arch'
+    Yast.import 'Linuxrc'
+    Yast.import 'Mode'
+    Yast.import 'Pkg'
+    Yast.import 'ProductFeatures'
 
-  extend FastGettext::Translation
+    extend FastGettext::Translation
 
-  DESCRIPTION = {
-    'id'              => 'services-manager',
-    'menu_title'      => N_("&Default systemd target and services"),
-    'rich_text_title' => N_("Default systemd target and services")
-  }
+    DESCRIPTION = {
+      'id'              => 'services-manager',
+      'menu_title'      => N_("&Default systemd target and services"),
+      'rich_text_title' => N_("Default systemd target and services")
+    }
 
-  def initialize
-    args = WFM.Args
-    function = args.shift.to_s
-    case function
-      when 'MakeProposal' then Proposal.new(args)
-      when 'AskUser'      then ask_user
-      when 'Description'  then DESCRIPTION
-      when 'Write'        then write
-    end
-  end
-
-  def ask_user
-  end
-
-  def write
-  end
-
-  class Proposal
-    module Type
-      GRAPHICAL = 'graphical'
-      MULTIUSER = 'multi-user'
+    def initialize
+      args = WFM.Args
+      function = args.shift.to_s
+      case function
+        when 'MakeProposal' then Proposal.create
+        when 'AskUser'      then ask_user
+        when 'Description'  then DESCRIPTION
+        when 'Write'        then write
+        else Builtins.y2error("Unknown function: %1", function)
+      end
     end
 
-    def initialize args
-      set_default_target
-      make_proposal
+    def ask_user
     end
 
-    private
+    def write
+      Builtins.y2milestone("Not writing yet, will be done in inst_finish")
+    end
 
-    #TODO
-    # Add warnings to the last 2 setups, add logging
-    # Create the proposal widget as in RunlevelEd
-    def set_default_target
-      if Mode.autoinst
-        # Look at the RunlevelProposal, around line 118 where this stops
-        # check if vnc is running
-        # check if ssh is running
-      else
-        if Arch.x11_setup_needed && Pkg.IsSelected("xorg-x11") || Mode.live_installation
-          default_target = Type::GRAPHICAL
+    class Proposal
+
+      def self.create
+        new.proposal
+      end
+
+      module Type
+        GRAPHICAL = 'graphical'
+        MULTIUSER = 'multi-user'
+      end
+
+      attr_accessor :default_target
+      attr_reader   :warnings, :featured_target
+
+      def initialize
+        @warnings = []
+        @default_target = ProductFeatures.GetFeature('globals', 'runlevel')
+        change_default_target
+        update_warnings
+      end
+
+      def proposal
+        { 'preformatted_proposal' => "<ul><li>#{default_target}</li></ul>" }
+      end
+
+      private
+
+      def change_default_target
+        detect_target unless Mode.autoinst
+        SystemdTarget.default_target = self.default_target
+      end
+
+      def detect_target
+        if Arch.x11_setup_needed && Pkg.IsSelected("xorg-x11-server") || Mode.live_installation
+          self.default_target = Type::GRAPHICAL
+          return
         end
+
         if Linuxrc.serial_console
-          default_target = Type::MULTIUSER
+          self.default_target = Type::MULTIUSER
         elsif Linuxrc.vnc && Linuxrc.usessh
-          if UI.GetDisplayInfo == 'TextMode'
-            default_target = Type::MULTIUSER
-          else
-            default_target = Type::GRAPHICAL
-          end
+          self.default_target = UI.GetDisplayInfo == 'TextMode' ? Type::MULTIUSER : Type::GRAPHICAL
         elsif Linuxrc.vnc
-          default_target = Type::GRAPHICAL
+          self.default_target = Type::GRAPHICAL
         elsif Linuxrc.usessh
-          default_target = Type::MULTIUSER
+          self.default_target = Type::MULTIUSER
         end
       end
-      Yast::SystemdTarget.default_target = default_target
-    end
 
-    def
+      def update_warnings
+        if Linuxrc.vnc && default_target != Type::GRAPHICAL
+          warnings << _('VNC needs graphical system to be available')
+        end
+      end
 
-    def make_proposal
-      installation_type =
     end
   end
-end
 
-ServicesManagerProposal.new
+  ServicesManagerProposal.new
+end
 42
