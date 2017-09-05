2ad05dc59a5d231fec2cf927590dc374f7c4119a
Martin Vidner <mvidner@suse.cz>
2017-09-05 10:32:02 +0200

    Measure test coverage with Coveralls

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,28 @@
-require 'rspec'
-
 srcdir = File.expand_path("../../src", __FILE__)
 y2dirs = ENV.fetch("Y2DIR", "").split(":")
 ENV["Y2DIR"] = y2dirs.unshift(srcdir).join(":")
 
+if ENV["COVERAGE"]
+  require "simplecov"
+  SimpleCov.start do
+    add_filter "/test/"
+  end
+
+  src_location = File.expand_path("../../src", __FILE__)
+  # track all ruby files under src
+  SimpleCov.track_files("#{src_location}/**/*.rb")
+
+  # use coveralls for on-line code coverage reporting at Travis CI
+  if ENV["TRAVIS"]
+    require "coveralls"
+    SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+      SimpleCov::Formatter::HTMLFormatter,
+      Coveralls::SimpleCov::Formatter
+    ]
+  end
+end
+
+require "rspec"
 require "yast"
 
 Yast.import 'ServicesManager'
