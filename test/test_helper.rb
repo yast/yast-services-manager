1e0b7f93af18fba4cb6890e49dcef04daa3b50f9
José Iván López González <jlopez@suse.com>
2018-07-09 12:24:18 +0100

    Add helpers for testing

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,39 @@
+# encoding: utf-8
+
+# Copyright (c) [2013-2018] SUSE LLC
+#
+# All Rights Reserved.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of version 2 of the GNU General Public License as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, contact SUSE LLC.
+#
+# To contact SUSE LLC about this file by physical or electronic mail, you may
+# find current contact information at www.suse.com.
+
 srcdir = File.expand_path("../../src", __FILE__)
 y2dirs = ENV.fetch("Y2DIR", "").split(":")
 ENV["Y2DIR"] = y2dirs.unshift(srcdir).join(":")
+
+require "rspec"
+require "yast"
+
+Yast.import 'ServicesManager'
+include Yast::Logger
+
+require_relative "support/services_manager_helpers"
+
+RSpec.configure do |c|
+  c.include Yast::RSpec::ServicesManagerHelpers
+end
 
 if ENV["COVERAGE"]
   require "simplecov"
@@ -21,9 +54,3 @@ if ENV["COVERAGE"]
     ]
   end
 end
-
-require "rspec"
-require "yast"
-
-Yast.import 'ServicesManager'
-include Yast::Logger
