bef8722b4bbe678eb920aaf45857021a54e21d61
José Iván López González <jlopez@suse.com>
2018-08-03 15:51:42 +0100

    Add ShowDetailsButton class

---
+++ src/lib/services-manager/widgets/show_details_button.rb
@@ -0,0 +1,61 @@
+# encoding: utf-8
+
+# Copyright (c) [2018] SUSE LLC
+#
+# All Rights Reserved.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of version 2 of the GNU General Public License as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, contact SUSE LLC.
+#
+# To contact SUSE LLC about this file by physical or electronic mail, you may
+# find current contact information at www.suse.com.
+
+require "yast"
+require "services-manager/widgets/base"
+
+Yast.import "ServicesManager"
+
+module Y2ServicesManager
+  module Widgets
+    # Button to show details about a service
+    class ShowDetailsButton < Base
+      extend Yast::I18n
+
+      textdomain "services-manager"
+
+      # Returns the plain libyui widget
+      #
+      # @return [Yast::Term]
+      def widget
+        PushButton(id, label)
+      end
+
+    private
+
+      # Default widget id
+      #
+      # @see Base#default_id
+      #
+      # @return [Yast::Term]
+      def default_id
+        Id(:show_details_button)
+      end
+
+      # Button label
+      #
+      # @return [String]
+      def label
+        _("Show &Details")
+      end
+    end
+  end
+end
